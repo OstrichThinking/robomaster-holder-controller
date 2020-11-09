f16906056eacdb485f83a8b213bed98143f26dee
zhuda <15187289230@163.com>
2020-11-09 17:57:16 +0800

    初步完成云台控制

---
+++ Bsp/bsp_can.c
@@ -0,0 +1,150 @@
+/****************************************************************************
+ *  Copyright (C) 2018 RoboMaster.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+ 
+#include "bsp_can.h"
+#include "stdio.h"
+
+
+moto_info_t motor_info1 = {0};
+moto_info_t motor_info2 = {0};
+moto_info_t motor_info3 = {0};
+moto_info_t motor_info4 = {0};
+
+uint16_t can_cnt;
+
+/**
+  * @brief  init can filter, start can, enable can rx interrupt
+  * @param  hcan pointer to a CAN_HandleTypeDef structure that contains
+  *         the configuration information for the specified CAN.
+  * @retval None
+  */
+void can_user_init(CAN_HandleTypeDef* hcan )
+{
+  CAN_FilterTypeDef  can_filter;
+
+  can_filter.FilterBank = 0;                       // filter 0
+  can_filter.FilterMode =  CAN_FILTERMODE_IDMASK;  // mask mode
+  can_filter.FilterScale = CAN_FILTERSCALE_32BIT;
+  can_filter.FilterIdHigh = 0x0000;
+  can_filter.FilterIdLow  = 0x0000;
+  can_filter.FilterMaskIdHigh = 0x0000;
+  can_filter.FilterMaskIdLow  = 0x0000;           // set mask 0 to receive all can id
+  can_filter.FilterFIFOAssignment = CAN_RX_FIFO0; // assign to fifo0
+  can_filter.FilterActivation = ENABLE;           // enable can filter
+  can_filter.SlaveStartFilterBank  = 14;          // only meaningful in dual can mode
+   
+  if(HAL_CAN_ConfigFilter(hcan,&can_filter) != HAL_OK){
+		Error_Handler();
+	    //printf("过滤器初始化失败");
+	}
+	if(HAL_CAN_ActivateNotification(hcan,CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK){
+		Error_Handler();
+	}
+	if(HAL_CAN_Start(hcan) != HAL_OK){
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief  can rx callback, get motor feedback info
+  * @param  hcan pointer to a CAN_HandleTypeDef structure that contains
+  *         the configuration information for the specified CAN.
+  * @retval None
+  */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    CAN_RxHeaderTypeDef rx_header;
+    uint8_t rx_data[8];
+	
+	if(hcan->Instance == CAN1){
+		HAL_CAN_GetRxMessage(hcan,CAN_RX_FIFO0,&rx_header,rx_data);
+	}
+	uint8_t index = rx_header.StdId - FEEDBACK_ID_BASE;  // get motor index by can_id
+	switch(index){
+		case 1:
+			can_cnt++;
+			motor_info1.rotor_angle    = ((rx_data[0] << 8) | rx_data[1]);
+//			motor_info1.rotor_angle = motor_info.rotor_angle / 22.75;
+			motor_info1.rotor_speed    = ((rx_data[2] << 8) | rx_data[3]);
+			motor_info1.torque_current = ((rx_data[4] << 8) | rx_data[5]);
+			motor_info1.temp           = ((rx_data[6] << 8) | rx_data[7]);
+//			printf("  2060-1机械角度  %2d ",    motor_info.rotor_angle);
+//			printf("  转速 %2d ",motor_info.rotor_speed);
+//			printf("  实际转矩电流 %2d ",    motor_info.torque_current);
+//			printf("  温度 %2d  \r\n",motor_info.temp);
+			break;
+		case 2:
+			can_cnt++;
+			motor_info2.rotor_angle    = ((rx_data[0] << 8) | rx_data[1]) / 22.75f; //换为0-360内的角度
+			motor_info2.rotor_speed    = ((rx_data[2] << 8) | rx_data[3]);
+			motor_info2.torque_current = ((rx_data[4] << 8) | rx_data[5]);
+			motor_info2.temp           = ((rx_data[6] << 8) | rx_data[7]);
+//			printf("  2060-2机械角度  %2d ",    motor_info.rotor_angle);
+//			printf("  转速 %2d ",motor_info.rotor_speed);
+//			printf("  实际转矩电流 %2d ",    motor_info.torque_current);
+//			printf("  温度 %2d  \r\n",motor_info.temp);
+			break;
+		case 3:
+			can_cnt++;
+			motor_info3.rotor_angle    = ((rx_data[0] << 8) | rx_data[1]);
+			motor_info3.rotor_speed    = ((rx_data[2] << 8) | rx_data[3]);
+			motor_info3.torque_current = ((rx_data[4] << 8) | rx_data[5]);
+			motor_info3.temp           = ((rx_data[6] << 8) | rx_data[7]);
+//			printf("  6060-3机械角度  %2d ",    motor_info.rotor_angle);
+//			printf("  转速 %2d ",motor_info.rotor_speed);
+//			printf("  实际转矩电流 %2d ",    motor_info.torque_current);
+//			printf("  温度 %2d  \r\n",motor_info.temp);
+			break;
+		case 4:
+			can_cnt++;
+			motor_info4.rotor_angle    = ((rx_data[0] << 8) | rx_data[1]) / 22.75f; //换为0-360内的角度
+			motor_info4.rotor_speed    = ((rx_data[2] << 8) | rx_data[3]);
+			motor_info4.torque_current = ((rx_data[4] << 8) | rx_data[5]);
+			motor_info4.temp           = ((rx_data[6] << 8) | rx_data[7]);
+			break;
+	}
+}
+
+/**
+  * @brief  send motor control message through can bus
+  * @param  id_range to select can control id 0x1ff or 0x2ff
+  * @param  motor voltage 1,2,3,4 or 5,6,7
+  * @retval None
+  */
+void set_motor_voltage(int16_t v1, int16_t v2, int16_t v3, int16_t v4)
+{
+  CAN_TxHeaderTypeDef tx_header;
+  uint8_t             tx_data[8];
+    
+  tx_header.StdId = 0x1ff;
+  tx_header.IDE   = CAN_ID_STD;
+  tx_header.RTR   = CAN_RTR_DATA;
+  tx_header.DLC   = 8;
+
+  tx_data[0] = (v1>>8)&0xff; //电机1
+  tx_data[1] =    (v1)&0xff;
+  tx_data[2] = (v2>>8)&0xff; //电机2
+  tx_data[3] =    (v2)&0xff;
+
+  tx_data[4] = (v3>>8)&0xff; //电机3
+  tx_data[5] =    (v3)&0xff;
+	
+  tx_data[6] = (v4>>8)&0xff; //电机4
+  tx_data[7] =    (v4)&0xff;
+
+	HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0); 
+}
---
+++ Core/Src/freertos.c
@@ -0,0 +1,241 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "bsp_can.h"
+#include "bsp_rc.h"
+#include "pid.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+extern UART_HandleTypeDef huart8;
+extern UART_HandleTypeDef huart6;
+
+extern rc_info_t rc;
+char buf[200];
+ 
+extern pid_struct_t motor_rotate2_pid;
+extern pid_struct_t motor_rotate4_pid;
+
+float target_speed;
+
+float target_angle_yaw = 0.0f; //ÔÆÌ¨½Ç¶È
+float target_angle_pitch = 0.0f;
+
+extern moto_info_t motor_info4;
+extern moto_info_t motor_info2;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+osThreadId my2060Task1Handle;
+osThreadId my2060Task2Handle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+void Start2060Task1(void const * argument);
+void Start2060Task2(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityIdle, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* definition and creation of my2060Task1 */
+  osThreadDef(my2060Task1, Start2060Task1, osPriorityIdle, 0, 128);
+  my2060Task1Handle = osThreadCreate(osThread(my2060Task1), NULL);
+
+  /* definition and creation of my2060Task2 */
+  osThreadDef(my2060Task2, Start2060Task2, osPriorityIdle, 0, 128);
+  my2060Task2Handle = osThreadCreate(osThread(my2060Task2), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  sprintf(buf, "CH1: %4d  CH2: %4d  CH3: %4d  CH4: %4d  SW1: %1d  SW2: %1d \r\n", rc.ch1, rc.ch2, rc.ch3, rc.ch4, rc.sw1, rc.sw2);
+	  HAL_UART_Transmit(&huart6, (uint8_t *)buf, (COUNTOF(buf) - 1), 55);
+	  osDelay(5);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_Start2060Task1 */
+/**
+* @brief Function implementing the my2060Task1 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_Start2060Task1 */
+void Start2060Task1(void const * argument)
+{
+  /* USER CODE BEGIN Start2060Task1 */
+  /* Infinite loop */
+  for(;;)
+  {
+	  osDelay(5);
+  }
+  /* USER CODE END Start2060Task1 */
+}
+
+/* USER CODE BEGIN Header_Start2060Task2 */
+/**
+* @brief Function implementing the my2060Task2 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_Start2060Task2 */
+void Start2060Task2(void const * argument)
+{
+  /* USER CODE BEGIN Start2060Task2 */
+  /* Infinite loop */
+  for(;;)
+  {
+	/* motor speed pid calc */
+	  
+//	  if(rc.sw2 == 1){
+//		  angle_yaw += rc.ch1 * 0.005;
+//		  angle_yaw = angle_yaw>180 ? 180 : (angle_yaw<0 ? 0 : angle_yaw);
+//		  float yaw = pid_calc(&motor_rotate4_pid, angle_yaw, motor_info.rotor_angle);
+//		  set_motor_voltage(0x0000,0x0000,0x0000,yaw);
+//	  }
+//	  if(rc.sw1 == 1){  
+//		  angle_pitch += rc.ch4 * 0.005;
+//		  angle_pitch = angle_pitch>250 ? 250 : (angle_pitch<180 ? 180: angle_pitch);
+//		  float pitch = pid_calc(&motor_rotate2_pid, angle_pitch, motor_info.rotor_angle);
+//		  set_motor_voltage(0x0000,pitch,0x0000,0x0000);
+//	  }
+	  
+	  
+	  if(rc.sw1 == 1 && rc.sw2 == 1){
+		  
+		  target_angle_yaw += rc.ch1 * 0.005;
+		  target_angle_yaw = target_angle_yaw>180 ? 180 : (target_angle_yaw<0 ? 0 : target_angle_yaw);
+		  motor_info4.set_voltage = pid_calc(&motor_rotate4_pid, target_angle_yaw, motor_info4.rotor_angle);
+		  
+		  target_angle_pitch += rc.ch4 * 0.005;
+		  target_angle_pitch = target_angle_pitch>250 ? 250 : (target_angle_pitch<180 ? 180: target_angle_pitch);
+		  motor_info2.set_voltage = pid_calc(&motor_rotate2_pid, target_angle_pitch, motor_info2.rotor_angle);
+		  
+		  
+		  set_motor_voltage(0x0000,motor_info2.set_voltage,0x0000,motor_info4.set_voltage);
+		  
+	  }
+	  
+	  osDelay(5);
+	  
+  }
+  /* USER CODE END Start2060Task2 */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
